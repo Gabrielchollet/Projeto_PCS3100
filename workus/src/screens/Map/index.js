f9a42b4334cdb91e3582f50620ec080d3e2bf8c8
viniciusvianadp <86125479+viniciusvianadp@users.noreply.github.com>
2021-07-11 20:38:01 -0300

    Map changes

--- workus/src/screens/Map/index.js
+++ workus/src/screens/Map/index.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native'
-import MapScreen from './MapScreen'
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View } from 'react-native';
+import MapScreen from './MapScreen';
+import * as Location from 'expo-location';
 
 export function Map() {
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+        })();
+    }, []);
+
     return (
         <View style={styles.container}>
             <MapScreen />
