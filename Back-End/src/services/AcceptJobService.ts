35169875a3ee352b3779dd004d8ae931986c0048
Gabriel Chollet <62822641+Gabrielchollet@users.noreply.github.com>
2021-07-15 14:40:10 -0300

    FIX: bug no Accept Job

--- Back-End/src/services/AcceptJobService.ts
+++ Back-End/src/services/AcceptJobService.ts
@@ -4,21 +4,22 @@ import { JobsRepositories } from "../repositories/JobsRepositories";
 import { UsersRepositories } from "../repositories/UsersRepositories";
 
 interface IJobRequest {
-    user_id: string;
-    job_id: string;
-  }
+  user_id: string;
+  job_id: string;
+}
 
 class AcceptJobService {
-    async execute({user_id, job_id} : IJobRequest) {
-        const usersRepositories = getCustomRepository(UsersRepositories);
-        const jobsRepositories = getCustomRepository(JobsRepositories);
-        /* o programa obtem o nome do usuario a partir do seu id */
-        const { name } = await usersRepositories.findOne(user_id);
-        /* o programa atualiza a coluna worker do job com o nome do empregado */
-        await jobsRepositories.update(job_id, {worker : name});
-        /** retorna o emprego que foi aceito */
-        return job;
-      }
-    }
-    
-    export { AcceptJobService }
+  async execute({ user_id, job_id }: IJobRequest) {
+    const usersRepositories = getCustomRepository(UsersRepositories);
+    const jobsRepositories = getCustomRepository(JobsRepositories);
+    /* o programa obtem o nome do usuario a partir do seu id */
+    const { name } = await usersRepositories.findOne(user_id);
+    /* o programa atualiza a coluna worker do job com o nome do empregado */
+    const job = await jobsRepositories.findOne(job_id);
+    await jobsRepositories.update(job_id, { worker: name });
+    /** retorna o emprego que foi aceito */
+    return job;
+  }
+}
+
+export { AcceptJobService }
